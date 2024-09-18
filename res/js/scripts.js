6125dfd1dce00daef9de5714c9e3d99eaa81e2ab
Chloe <chloe.martins@etec.sp.gov.br>
2024-09-18 08:54:40 -0300

    Nunca mais vou tocar nesse negócio na minha vida

--- res/js/scripts.js
+++ res/js/scripts.js
@@ -52,3 +52,6 @@ if (toastDislikeTrigger) {
       toastBootstrap.show()
     })
 }
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
